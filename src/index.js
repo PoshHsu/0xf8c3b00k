3c3cceab763640443f5fd6fa63294303afca0590
Patrick Wang <kk1fff@gmail.com>
2012-04-03 00:19:08 +0800

    1. Store expiration timestamp of access token. 2. Implement basic command line handling mechinism.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
-var fbOauthClient = require('./fb_oauth_client.js');
+// Command format:
+// ./0xfb [Program option] <Command> [Command argument]
 
-fbOauthClient.getAuthToken(function(access_token, expires_in) {
-  console.log("We got token:");
-  console.log("  access_token: %s", access_token);
-  console.log("  expires_in: %s", expires_in);
-  process.exit(0);
+
+var CONFIG = require('./config.js').app_config();
+
+// prog_command:
+// Map command to module that run the command.
+// Each module must has run() function. And the first argument of this
+// function would be program option, while the second one would be
+// command argument. Move to ./config.js ?
+var progCommand = {
+  // Update photo
+  'uploadphoto' : './upload_photo.js'
+};
+
+// It is a chance for program to make global setting before and command
+// is issued. Command will be executed right after this step.
+var handleSetting = function(progOpt, command, cmdArg) {
+  // So far it's empty XD.
+}
+
+// Parse command line, collect program option, command, command argrment.
+var initRun = function() {
+
+  var program_option = [];
+  var command = '';
+  var command_argument = [];
+
+  // [TODO Patrick] Here I assume there is no program options, so we just
+  // use that first argument as command
+
+  command = process.argv[2];
+  command_argument = process.argv.slice(3);
+
+  if (!progCommand.hasOwnProperty(command)) {
+    // Invalid command.
+    process.exit(1);
+  }
+
+  handleSetting(program_option, command, command_argument);
+
+  // Go
+  require(progCommand[command]).run(program_option, command_argument);
+}
+
+// *** Start with fetching access token ***
+var fbOauthClient = require('./fb_oauth_client.js');
+fbOauthClient.getAuthToken(function(access_token, expires) {
+  // Debug !!
+  console.log(" Access token: %s", access_token);
+  console.log(" Expire Time: %d, now: %d", expires, parseInt(Date.now()/1000));
+
+  initRun();
 });
---
+++ src/upload_photo.js
@@ -0,0 +1,5 @@
+exports.run = function(progOpt, cmdArgs) {
+  console.log(progOpt);
+  console.log(cmdArgs);
+  process.exit(0);
+}
